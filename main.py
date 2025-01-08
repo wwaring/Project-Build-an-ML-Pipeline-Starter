58bbde8a2017a514b0803e54fe5ae598ba62eabe
Wendy Waring <wndichu@wgu.edu>
2025-01-08 16:11:44 -0500

    Updated main.py to ensure config is JSON-serializable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tempfile
 import os
 import wandb
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 _steps = [
     "download",
@@ -102,6 +102,8 @@ def go(config: DictConfig):
             print("Random forest config:", config["modeling"]["random_forest"])
 
             # NOTE: we need to serialize the random forest configuration into JSON
+            # Troubleshoot: Ensure config is JSON-serializable
+            config["modeling"]["random_forest"] = OmegaConf.to_container(config["modeling"]["random_forest"], resolve=True)
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
